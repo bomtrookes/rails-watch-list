8c7303663edffbfd674068e659b32eaac3d13728
Tom Brookes <tom.brookes15@gmail.com>
2022-05-03 19:55:12 +0100

    making progress

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+
+puts "Creating Movies..."
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "Movies Created..."
+
+puts "Creating Lists..."
+List.create(name: "Favourite Movies Ever")
+List.create(name: "10 Movies To Watch When It's Raining")
+List.create(name: "Tarantino Tuesday Night")
+List.create(name: "100 Films To See Before You Die")
+puts "Lists Created..."
+
+puts "Creating Bookmarks..."
+Bookmark.create!(comment: "Harry said it was good", movie: Movie.all.sample, list: List.all.sample)
+Bookmark.create(comment: "Saw this on buzzfeed", movie: Movie.all.sample, list: List.all.sample)
+Bookmark.create(comment: "Yet to actually watch these...", movie: Movie.all.sample, list: List.all.sample)
+Bookmark.create(comment: "Sounded good", movie: Movie.all.sample, list: List.all.sample)
+puts "Bookmarks Created..."
+
+# chain commands in the terminal using '&&'
+# rails db:drop && rails db:create && rails db:migrate && rails db:seed
